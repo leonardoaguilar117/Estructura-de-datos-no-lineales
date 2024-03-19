553a7a12928f14e2cc7ad9c0b043652a7efc1106
Leonardo Aguilar Martínez <leonardogrl18@gmail.com>
2024-03-18 20:20:52 -0600

    Nueva tarea

--- Practica 2/Estruturas no lineales/src/PruebaDeTiempo.java
+++ Practica 2/Estruturas no lineales/src/PruebaDeTiempo.java
@@ -1,188 +1,133 @@
 
 public class PruebaDeTiempo {
 	
-	
-	public void Insercion(int n){
-		Lista L = new Lista();
-		Pila P = new Pila();
-		Cola C = new Cola();
+	public PruebaDeTiempo() {
 		
+	}
+	
+	public void operList(int n) {
+		double inicio, fin, res; 
+		Lista L = new Lista();
+		
+		inicio = System.nanoTime();
 		for(int i=0; i<n; i++) {
 			L.insertar(i);
-			P.push(i);
-			C.encolar(i);
 		}
-	}
-	
-	public void Eliminar(int n){
-		Lista L = new Lista();
-		Pila P = new Pila();
-		Cola C = new Cola();
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Insertar en lista con "+ n +" elementos: "+res);
 		
-		for(int i=0; i<n; i++) {
-			L.eliminar(i);
-			P.pop();
-			C.desencolar();
-		}
-	}
-	
-	public void Busqueda(int n) {
-		Lista L = new Lista();
-		Pila P = new Pila();
-		Cola C = new Cola();
 		
+		inicio = System.nanoTime();
 		for(int i=0; i<n; i++) {
 			L.buscar(i);
-			P.buscar(i);
-			C.buscarElemento(i);
 		}
-	}
-	
-	public void Acceso(int n){
-		Lista L = new Lista();
-		Pila P = new Pila();
-		Cola C = new Cola();
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Buscar en lista con "+ n +" elementos: "+res);
 		
+		inicio = System.nanoTime();
 		for(int i=0; i<n; i++) {
 			L.acceder(i);
-			P.acceder(i);
-			C.acceder(i);
 		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Acceder en lista con "+ n +" elementos: "+res);
+		
+		inicio = System.nanoTime();
+		for(int i=0; i<n; i++) {
+			L.eliminar(i);
+		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Eliminar en lista con "+ n +" elementos: "+res);	
 	}
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args) {
-		PruebaDeTiempo P1 = new PruebaDeTiempo();
-		long inicio, Final, total;
-		
-		//Operaciones con 50 elementos
-		inicio = System.nanoTime();
-		P1.Insercion(50);
-		Final = System.nanoTime();
-		total = (Final - inicio);
-		System.out.println("Insercion, tiempo: "+total+" nano segundos con 50 elementos");
-	
-		//Operaciones con 50 elementos
-		inicio = System.nanoTime();
-		P1.Acceso(50);
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Acceso, tiempo: "+total+" nano segundos con 50 elementos");
-				
-		//Operaciones con 50 elementos
-		inicio = System.nanoTime();
-		P1.Busqueda(50);;
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Busqueda, tiempo: "+total+" nano segundos con 50 elementos");
-					
-		//Operaciones con 50 elementos
-		inicio = System.nanoTime();
-		P1.Eliminar(50);;
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Eliminacion, tiempo: "+total+" nano segundos con 50 elementos");
-		
-		System.out.println(" ");
-		
-		//------------------------------------ 500 -------------------------------------
 
-		//Operaciones con 500 elementos
-		inicio = System.nanoTime();
-		P1.Insercion(500);
-		Final = System.nanoTime();
-		total = (Final - inicio);
-		System.out.println("Insercion, tiempo: "+total+" nano segundos con 500 elementos");
-			
-		//Operaciones con 500 elementos
-		inicio = System.nanoTime();
-		P1.Acceso(500);
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Acceso, tiempo: "+total+" nano segundos con 500 elementos");
-						
-		//Operaciones con 500 elementos
-		inicio = System.nanoTime();
-		P1.Busqueda(500);;
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Busqueda, tiempo: "+total+" nano segundos con 500 elementos");
-							
-		//Operaciones con 500 elementos
-		inicio = System.nanoTime();
-		P1.Eliminar(500);;
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Eliminacion, tiempo: "+total+" nano segundos con 500 elementos");
-				
-		System.out.println(" ");
+	public void operCola(int n) {
+		double inicio, fin, res; 
+		Cola L = new Cola();
 		
-	
-		//------------------------------------ 5000 -------------------------------------
-		//Operaciones con 5000 elementos
 		inicio = System.nanoTime();
-		P1.Insercion(5000);
-		Final = System.nanoTime();
-		total = (Final - inicio);
-		System.out.println("Insercion, tiempo: "+total+" nano segundos con 5000 elementos");
-					
-		//Operaciones con 5000 elementos
-		inicio = System.nanoTime();
-		P1.Acceso(5000);
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-				System.out.println("Acceso, tiempo: "+total+" nano segundos con 5000 elementos");
-								
-		//Operaciones con 5000 elementos
-		inicio = System.nanoTime();
-		P1.Busqueda(5000);
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Busqueda, tiempo: "+total+" nano segundos con 5000 elementos");
-									
-		//Operaciones con 5000 elementos
-		inicio = System.nanoTime();
-		P1.Eliminar(5000);
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Eliminacion, tiempo: "+total+" nano segundos con 5000 elementos");
-						
-		System.out.println(" ");
+		for(int i=0; i<n; i++) {
+			L.encolar(i);
+		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Insertar en cola con "+ n+" elementos: "+res);
 		
-		//------------------------------------ 50000 -------------------------------------
-		//Operaciones con 50000 elementos
+		
 		inicio = System.nanoTime();
-		P1.Insercion(50000);
-		Final = System.nanoTime();
-		total = (Final - inicio);
-		System.out.println("Insercion, tiempo: "+total+" nano segundos con 50000 elementos");
-							
-		//Operaciones con 50000 elementos
+		for(int i=0; i<n; i++) {
+			L.buscarElemento(i);
+		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Buscar en cola con "+ n+" elementos: "+res);
+		
 		inicio = System.nanoTime();
-		P1.Acceso(50000);
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Acceso, tiempo: "+total+" nano segundos con 50000 elementos");
-										
-		//Operaciones con 50000 elementos
+		for(int i=0; i<n; i++) {
+			L.acceder(i);
+		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Acceder en cola con "+ 50+" elementos: "+res);
+		
 		inicio = System.nanoTime();
-		P1.Busqueda(50000);
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Busqueda, tiempo: "+total+" nano segundos con 50000 elementos");
-											
-		//Operaciones con 5000 elementos
-		inicio = System.nanoTime();
-		P1.Eliminar(50000);
-		Final = System.nanoTime();			
-		total = (Final - inicio);
-		System.out.println("Eliminacion, tiempo: "+total+" nano segundos con 50000 elementos");
-								
-		System.out.println(" ");
-	
+		for(int i=0; i<n; i++) {
+			L.desencolar();
+		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Borrar en cola con "+ n+" elementos: "+res);	
 	}
+	
+	public void operPila(int n) {
+		double inicio, fin, res; 
+		Pila L = new Pila();
+		
+		inicio = System.nanoTime();
+		for(int i=0; i<n; i++) {
+			L.push(i);
+		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Insertar en pila con "+ n +" elementos: "+res);
+		
+		
+		inicio = System.nanoTime();
+		for(int i=0; i<n; i++) {
+			L.buscar(i);
+		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Buscar en pila con "+ n+" elementos: "+res);
+		
+		inicio = System.nanoTime();
+		for(int i=0; i<n; i++) {
+			L.acceder(i);
+		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Acceder en pila con "+ n+" elementos: "+res);
+		
+		inicio = System.nanoTime();
+		for(int i=0; i<n; i++) {
+			L.pop();
+		}
+		fin = System.nanoTime();
+		res = fin - inicio;
+		System.out.println("Borrar en pila con "+ n+" elementos: "+res);	
+	}
+
+	public static void main(String args[]) {
+		PruebaDeTiempo P = new PruebaDeTiempo();
+		
+		P.operList(50000);
+		System.out.println(" ");
+		P.operCola(50000);
+		System.out.println(" ");
+		P.operPila(50000);
+		
+	}
+	
 }
--- Practica 2/Estruturas no lineales/src/PruebaDeTiempoT.java
+++ Practica 2/Estruturas no lineales/src/PruebaDeTiempoT.java
@@ -1,194 +1,145 @@
-
 public class PruebaDeTiempoT<T> {
-	
-	private T crearElemento(int valor) {
-	    return (T) (Integer.valueOf(valor)); 
-	}
-	
-	public void Insercion(int n) {
-	    ListaT<T> L = new ListaT<>();
-	    PilaT<T> P = new PilaT<>();
-	    ColaT<T> C = new ColaT<>();
-	    
-	    for (int i = 0; i < n; i++) {
-	    	T elemento = crearElemento(i);
-	        L.insertar(elemento); // Aquí deberías proporcionar el valor de tipo T que deseas insertar
-	        P.push(elemento); // Aquí también
-	        C.encolar(elemento); // Igualmente aquí
-	    }
-	}
+    
+    public PruebaDeTiempoT() {
+        
+    }
+    
+    public void operList(int n) {
+        double inicio, fin, res; 
+        ListaT<T> L = new ListaT<>();
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Insertar elementos en la lista
+            L.insertar((T) Integer.valueOf(i)); // Suponiendo que la lista toma elementos de tipo Integer
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Insertar en lista con "+ n +" elementos: "+res);
+        
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Buscar elementos en la lista
+            L.buscar((T) Integer.valueOf(i)); // Suponiendo que la lista toma elementos de tipo Integer
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Buscar en lista con "+ n +" elementos: "+res);
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Acceder a elementos en la lista
+            L.acceder(i);
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Acceder en lista con "+ n +" elementos: "+res);
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Eliminar elementos en la lista
+            L.eliminar(i); // Suponiendo que la lista toma elementos de tipo Integer
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Eliminar en lista con "+ n +" elementos: "+res);    
+    }
 
-	public void Eliminar(int n) {
-	    ListaT<T> L = new ListaT<>();
-	    PilaT<T> P = new PilaT<>();
-	    ColaT<T> C = new ColaT<>();
-	    
-	    for (int i = 0; i < n; i++) {
-	        L.eliminar(i); // Aquí deberías proporcionar el valor de tipo T que deseas eliminar
-	        P.pop();
-	        C.desencolar();
-	    }
-	}
+    public void operCola(int n) {
+        double inicio, fin, res; 
+        ColaT<T> L = new ColaT<>();
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Encolar elementos
+            L.encolar((T) Integer.valueOf(i)); // Suponiendo que la cola toma elementos de tipo Integer
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Insertar en cola con "+ n+" elementos: "+res);
+        
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Buscar elementos en la cola
+            L.buscarElemento((T) Integer.valueOf(i)); // Suponiendo que la cola toma elementos de tipo Integer
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Buscar en cola con "+ n+" elementos: "+res);
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Acceder a elementos en la cola
+            L.acceder(i);
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Acceder en cola con "+ 50+" elementos: "+res);
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Desencolar elementos
+            L.desencolar();
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Borrar en cola con "+ n+" elementos: "+res);    
+    }
+    
+    public void operPila(int n) {
+        double inicio, fin, res; 
+        PilaT<T> L = new PilaT<>();
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Insertar elementos en la pila
+            L.push((T) Integer.valueOf(i)); // Suponiendo que la pila toma elementos de tipo Integer
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Insertar en pila con "+ n +" elementos: "+res);
+        
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Buscar elementos en la pila
+            L.buscar((T) Integer.valueOf(i)); // Suponiendo que la pila toma elementos de tipo Integer
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Buscar en pila con "+ n+" elementos: "+res);
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Acceder a elementos en la pila
+            L.acceder(i);
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Acceder en pila con "+ n+" elementos: "+res);
+        
+        inicio = System.nanoTime();
+        for(int i=0; i<n; i++) {
+            // Desapilar elementos
+            L.pop();
+        }
+        fin = System.nanoTime();
+        res = fin - inicio;
+        System.out.println("Borrar en pila con "+ n+" elementos: "+res);    
+    }
 
-	public void Busqueda(int n) {
-	    ListaT<T> L = new ListaT<>();
-	    PilaT<T> P = new PilaT<>();
-	    ColaT<T> C = new ColaT<>();
-	    
-	    for (int i = 0; i < n; i++) {
-	    	
-	    	T elemento = crearElemento(i);
-	        L.buscar(elemento); // Aquí deberías proporcionar el valor de tipo T que deseas buscar
-	        P.buscar(elemento); // Aquí también
-	        C.buscarElemento(elemento); // Igualmente aquí
-	    }
-	}
-
-	public void Acceso(int n) {
-	    ListaT<T> L = new ListaT<>();
-	    PilaT<T> P = new PilaT<>();
-	    ColaT<T> C = new ColaT<>();
-	    
-	    for (int i = 0; i < n; i++) {
-	        L.acceder(i); // Aquí deberías proporcionar el índice de tipo T al que deseas acceder
-	        P.acceder(i); // Aquí también
-	        C.acceder(i); // Igualmente aquí
-	    }
-	}
-
-
-	
-	public static void main(String[] args) {
-		PruebaDeTiempoT<Integer> P1 = new PruebaDeTiempoT<>();
-		long inicio, Final, total;
-		
-		
-		//Operaciones con 50 elementos
-				inicio = System.nanoTime();
-				P1.Insercion(50);
-				Final = System.nanoTime();
-				total = (Final - inicio);
-				System.out.println("Insercion, tiempo: "+total+" nano segundos con 50 elementos");
-			
-				//Operaciones con 50 elementos
-				inicio = System.nanoTime();
-				P1.Acceso(50);
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Acceso, tiempo: "+total+" nano segundos con 50 elementos");
-						
-				//Operaciones con 50 elementos
-				inicio = System.nanoTime();
-				P1.Busqueda(50);;
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Busqueda, tiempo: "+total+" nano segundos con 50 elementos");
-							
-				//Operaciones con 50 elementos
-				inicio = System.nanoTime();
-				P1.Eliminar(50);;
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Eliminacion, tiempo: "+total+" nano segundos con 50 elementos");
-				
-				System.out.println(" ");
-				
-				//------------------------------------ 500 -------------------------------------
-
-				//Operaciones con 500 elementos
-				inicio = System.nanoTime();
-				P1.Insercion(500);
-				Final = System.nanoTime();
-				total = (Final - inicio);
-				System.out.println("Insercion, tiempo: "+total+" nano segundos con 500 elementos");
-					
-				//Operaciones con 500 elementos
-				inicio = System.nanoTime();
-				P1.Acceso(500);
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Acceso, tiempo: "+total+" nano segundos con 500 elementos");
-								
-				//Operaciones con 500 elementos
-				inicio = System.nanoTime();
-				P1.Busqueda(500);;
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Busqueda, tiempo: "+total+" nano segundos con 500 elementos");
-									
-				//Operaciones con 500 elementos
-				inicio = System.nanoTime();
-				P1.Eliminar(500);;
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Eliminacion, tiempo: "+total+" nano segundos con 500 elementos");
-						
-				System.out.println(" ");
-				
-			
-				//------------------------------------ 5000 -------------------------------------
-				//Operaciones con 5000 elementos
-				inicio = System.nanoTime();
-				P1.Insercion(5000);
-				Final = System.nanoTime();
-				total = (Final - inicio);
-				System.out.println("Insercion, tiempo: "+total+" nano segundos con 5000 elementos");
-							
-				//Operaciones con 5000 elementos
-				inicio = System.nanoTime();
-				P1.Acceso(5000);
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-						System.out.println("Acceso, tiempo: "+total+" nano segundos con 5000 elementos");
-										
-				//Operaciones con 5000 elementos
-				inicio = System.nanoTime();
-				P1.Busqueda(5000);
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Busqueda, tiempo: "+total+" nano segundos con 5000 elementos");
-											
-				//Operaciones con 5000 elementos
-				inicio = System.nanoTime();
-				P1.Eliminar(5000);
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Eliminacion, tiempo: "+total+" nano segundos con 5000 elementos");
-								
-				System.out.println(" ");
-				
-				//------------------------------------ 50000 -------------------------------------
-				//Operaciones con 50000 elementos
-				inicio = System.nanoTime();
-				P1.Insercion(50000);
-				Final = System.nanoTime();
-				total = (Final - inicio);
-				System.out.println("Insercion, tiempo: "+total+" nano segundos con 50000 elementos");
-									
-				//Operaciones con 50000 elementos
-				inicio = System.nanoTime();
-				P1.Acceso(50000);
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Acceso, tiempo: "+total+" nano segundos con 50000 elementos");
-												
-				//Operaciones con 50000 elementos
-				inicio = System.nanoTime();
-				P1.Busqueda(50000);
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Busqueda, tiempo: "+total+" nano segundos con 50000 elementos");
-													
-				//Operaciones con 5000 elementos
-				inicio = System.nanoTime();
-				P1.Eliminar(50000);
-				Final = System.nanoTime();			
-				total = (Final - inicio);
-				System.out.println("Eliminacion, tiempo: "+total+" nano segundos con 50000 elementos");
-										
-				System.out.println(" ");
-		
-		
-	}
-
+    public static void main(String args[]) {
+        PruebaDeTiempoT<Integer> P = new PruebaDeTiempoT<>();
+        
+        P.operList(5000);
+        System.out.println(" ");
+        P.operCola(5000);
+        System.out.println(" ");
+        P.operPila(5000);
+        
+    }
+    
 }
+
